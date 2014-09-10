5180ef73503c0041d4ab6ae3ca13be1000be1ac3
Keerthan Jaic <jckeerthan@gmail.com>
2014-09-10 01:00:33 -0400

    Initial commit. Working box

---
+++ setup/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TI_MSPGCC_DIR=/opt/ti-mspgcc
+/vagrant/setup/msp430-gcc-full-linux-installer-2.1.1.0.run --mode unattended --prefix $TI_MSPGCC_DIR
+echo "export PATH=$TI_MSPGCC_DIR/bin:$PATH" >> /etc/profile
+
+apt-get install -y mspdebug
+ln -s $TI_MSPGCC_DIR/bin/libmsp430.so /usr/lib/
