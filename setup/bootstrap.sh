8cb194ca850e02e42d8b26f67a5b2d8af5495810
Keerthan Jaic <jckeerthan@gmail.com>
2014-09-12 02:39:13 -0400

    Install extra kernel modules since 5969 launchpad needs serial acces for programming

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -5,5 +5,5 @@ TI_MSPGCC_DIR=/opt/ti-mspgcc
 echo "export PATH=$TI_MSPGCC_DIR/bin:$PATH" >> /etc/profile
 $TI_MSPGCC_DIR/install_scripts/msp430uif_install.sh
 
-apt-get install -y mspdebug
+apt-get install -y mspdebug linux-image-extra-virtual
 ln -s $TI_MSPGCC_DIR/bin/libmsp430.so /usr/lib/
